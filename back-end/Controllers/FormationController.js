ecfe123da14dc27d9d536b556270df49302e5e0c
soumiaessabiry <soumiaessabiry.se@gmail.com>
2023-01-06 13:03:31 +0100

    Ajoute function update formtion

--- back-end/Controllers/FormationController.js
+++ back-end/Controllers/FormationController.js
@@ -23,8 +23,23 @@ const ApiError=require("../middlewares/CatchError")
     })
 
  }
-
+ const UpdateFormation=async(req,res)=>{
+    const update_formation=await formationModel.findOneAndUpdate({_id:req.params.id},{
+        $set:{
+            Name_Formation:req.body.Name_Formation,
+            Image_formation:req.body.Image_formation,
+            Date_debut:req.body.Date_debut,
+            Date_Fin:req.body.Date_Fin,
+            Desciption:req.body.Desciption,
+        }})
+        if(update_formation){
+            res.status(201).json({update_formation})
+        }else{
+            throw Error("Error to update Formation")
+        }
+ }
 
 module.exports={
-    Ajouterformation
+    Ajouterformation,
+    UpdateFormation
 }
--- back-end/Routes/AdminRouter/FormationRoute.js
+++ back-end/Routes/AdminRouter/FormationRoute.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const router=express.Router()
-const {Ajouterformation}=require("../../Controllers/FormationController")
+const {Ajouterformation,UpdateFormation}=require("../../Controllers/FormationController")
 const CatchError=require("../../middlewares/CatchError")
 const errorHandller=require("../../middlewares/ErrorHandler")
 
 router.post('/AjouterFormation',CatchError(Ajouterformation))
+router.put('/Updateformation/:id',CatchError(UpdateFormation))
 router.use(errorHandller)
 
 module.exports=router
