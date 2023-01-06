5d2c3ac71c2d4d0b3aba1336eca5ed7b600052a6
soumiaessabiry <soumiaessabiry.se@gmail.com>
2023-01-06 12:47:29 +0100

    Ajoute function delet organisme

--- back-end/Controllers/OrganismeController.js
+++ back-end/Controllers/OrganismeController.js
@@ -18,19 +18,30 @@ const Ajouterorganisme=async(req,res)=>{
     })
 }
 const UpdateOrganisme=async(req,res)=>{
-
     const Update_Organisme=await OrganismeModel.findOneAndUpdate({_id:req.params.id},{$set:{name_organisme:req.body.name_organisme,ville:req.body.ville,phone:req.body.phone}})
     .then((Update_Organisme)=>{
         res.status(200).json({Update_Organisme})
     })
     .catch((err)=>{
         throw Error("Error to Update Organisme")
-
-    })
-
-    
+    })    
 }
+const DeletOrganisme=async(req,res)=>{
+        const Delet_Organisme=await OrganismeModel.findOneAndRemove({_id:req.params.id})
+        if(Delet_Organisme){
+            res.status(200).send("Success Organisme delete ")
+        }else{
+            throw Error("Error to delete Organisme")
+
+        }
+        
+}
+
+
+
+
 module.exports={
     Ajouterorganisme,
-    UpdateOrganisme
+    UpdateOrganisme,
+    DeletOrganisme
 }
--- back-end/Routes/AdminRouter/OrganismRoute.js
+++ back-end/Routes/AdminRouter/OrganismRoute.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const {Ajouterorganisme,UpdateOrganisme}=require("../../Controllers/OrganismeController")
+const {Ajouterorganisme,UpdateOrganisme,DeletOrganisme}=require("../../Controllers/OrganismeController")
 const CatchError=require("../../middlewares/CatchError")
 const errorHandller=require("../../middlewares/ErrorHandler")
 
 router.post('/Ajouterorganisme',CatchError(Ajouterorganisme))
-router.get('/Updateorganisme/:id',CatchError(UpdateOrganisme))
+router.put('/Updateorganisme/:id',CatchError(UpdateOrganisme))
+router.delete('/Deletorganisme/:id',CatchError(DeletOrganisme))
 router.use(errorHandller)
 
 module.exports=router
